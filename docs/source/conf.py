9967c7404e1e76b037293f51b5380aa0ec1d9f41
Karvy-Singh <karvysingh14@gmail.com>
2026-03-31 12:43:47 +0530

    chore: docs on github pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Agentic Ricing'
-copyright = '2026, Harsh Jha, Karvy Singh, Harsh Sharma, Rudra Kumar Singh'
-author = 'Harsh Jha, Karvy Singh, Harsh Sharma, Rudra Kumar Singh'
+project = "Agentic Ricing"
+copyright = "2026, Harsh Jha, Karvy Singh, Harsh Sharma, Rudra Kumar Singh"
+author = "Harsh Jha, Karvy Singh, Harsh Sharma, Rudra Kumar Singh"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
+
+html_baseurl = "https://wowtuff.github.io/ricing/"
